19fce20f0cd476e3d5bfd4dc4d678557bd671acc
Asiqul Islam <asiq.dev@gmail.com>
2025-04-07 12:49:49 +0600

    apps created and urls set

---
+++ dsa_tutor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DsaTutorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dsa_tutor'
--- personal_study_partner/settings.py
+++ personal_study_partner/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'dsa_tutor.apps.DsaTutorConfig',
+    'todo.apps.TodoConfig',
+    'weather.apps.WeatherConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,13 @@ WSGI_APPLICATION = 'personal_study_partner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ai_study_partner',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'PORT': '5432',
+        'HOST': 'localhost',
+
     }
 }
 
