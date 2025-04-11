1b9acfb547abf8ee05d176b93985da0dc988a7dd
Asiqul Islam <asiq.dev@gmail.com>
2025-04-11 12:55:02 +0600

    modified and ready to deploy server. added whitenoise for static files and wsgi.py file modified

--- personal_study_partner/settings.py
+++ personal_study_partner/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p^mn#iv&qj(5xegoh9#b*v+@c)ur0$@l#b5l27qv!sk-3wnb7b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- personal_study_partner/wsgi.py
+++ personal_study_partner/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_study_partner.settings')
+from personal_study_partner.settings import BASE_DIR
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
